9d768dfc192bb6f2ef9c14c4f56c9c7731e55254
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-06-09 14:13:48 +0200

    Add initial commit

---
+++ src/main/java/com/boardgamesworld/bgrental/BgRentalApplication.java
@@ -0,0 +1,13 @@
+package com.boardgamesworld.bgrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardgamesworld/bgrental/BgRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgamesworld.bgrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
