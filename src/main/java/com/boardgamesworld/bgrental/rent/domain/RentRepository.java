e8fe2e60a99f600c89aa4e09c060f8cfd822ca9c
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-07-13 16:19:07 +0200

    Add rent repository interface

---
+++ src/main/java/com/boardgamesworld/bgrental/rent/domain/RentRepository.java
@@ -0,0 +1,13 @@
+package com.boardgamesworld.bgrental.rent.domain;
+
+import java.util.List;
+
+public interface RentRepository {
+
+    void addRent(Rent rent);
+
+    List<Long> getAllRentBoardGameIds();
+
+    List<Long> getAllRentBoardGameIdsByUser(long userId);
+
+}
