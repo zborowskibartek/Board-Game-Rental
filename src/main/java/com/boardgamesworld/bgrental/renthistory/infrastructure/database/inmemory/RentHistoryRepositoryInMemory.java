d40041de97a1cff453325465ca2d7fc1fc39f8d3
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-07-14 01:56:13 +0200

    Add interface rent history repository and its implementation

---
+++ src/main/java/com/boardgamesworld/bgrental/renthistory/infrastructure/database/inmemory/RentHistoryRepositoryInMemory.java
@@ -0,0 +1,30 @@
+package com.boardgamesworld.bgrental.renthistory.infrastructure.database.inmemory;
+
+import com.boardgamesworld.bgrental.renthistory.domain.RentHistory;
+import com.boardgamesworld.bgrental.renthistory.domain.RentHistoryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RentHistoryRepositoryInMemory implements RentHistoryRepository {
+
+    private final List<RentHistory> rentHistories = new ArrayList<>();
+
+    @Override
+    public void addRentHistory(RentHistory rentHistory) {
+        rentHistories.add(rentHistory);
+    }
+
+    @Override
+    public List<RentHistory> getAllRentHistory() {
+        return rentHistories;
+    }
+
+    @Override
+    public List<RentHistory> getAllRentHistoryByUser(long userId) {
+        return rentHistories.stream()
+                .filter(rentHistory -> rentHistory.getUserId() == userId)
+                .collect(Collectors.toList());
+    }
+}
