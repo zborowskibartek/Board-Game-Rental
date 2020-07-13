1bbcc44433f1dcea9b0d63c428a2d233643c7521
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-07-13 16:11:58 +0200

    Add rent model and rent dto

--- src/main/java/com/boardgamesworld/bgrental/rent/infrastructure/api/RentDto.java
+++ src/main/java/com/boardgamesworld/bgrental/rent/infrastructure/api/RentDto.java
@@ -1,19 +1,21 @@
-package com.boardgamesworld.bgrental.rent.domain;
+package com.boardgamesworld.bgrental.rent.infrastructure.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Rent {
+public class RentDto {
 
     private long userId;
     private long gameId;
     private LocalDateTime rentedDate;
-    private LocalDateTime returnedDate = null;
+    private LocalDateTime returnedDate;
 
 }
