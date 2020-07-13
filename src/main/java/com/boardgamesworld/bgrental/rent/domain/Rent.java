ede3a2f1732f5fc3cee7d33cb689f8470a21d7a4
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-07-13 16:21:13 +0200

    Change rent atributes as a final fields

--- src/main/java/com/boardgamesworld/bgrental/rent/domain/Rent.java
+++ src/main/java/com/boardgamesworld/bgrental/rent/domain/Rent.java
@@ -2,18 +2,16 @@ package com.boardgamesworld.bgrental.rent.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class Rent {
 
-    private long userId;
-    private long gameId;
-    private LocalDateTime rentedDate;
-    private LocalDateTime returnedDate = null;
+    private final long userId;
+    private final long gameId;
+    private final LocalDateTime rentedDate;
+    private final LocalDateTime returnedDate = null;
 
 }
