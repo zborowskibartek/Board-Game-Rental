40299d529d5da303ad1445c9e0bda7a0879ac589
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-09-25 18:30:21 +0200

    Delete comments and spacing

--- src/main/java/com/boardgamesworld/bgrental/boardgame/domain/BoardGameFacadeConfiguration.java
+++ src/main/java/com/boardgamesworld/bgrental/boardgame/domain/BoardGameFacadeConfiguration.java
@@ -14,8 +14,5 @@ class BoardGameFacadeConfiguration {
         BoardGameService boardGameService = new BoardGameService(boardGameRepository, boardGameValidator);
 
         return new BoardGameFacade(boardGameService);
-
     }
-
-    //test
 }
